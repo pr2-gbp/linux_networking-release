c3dda25954da7328eef2129ea783346246df8c17
David Feil-Seifer <david.feilseifer@gmail.com>
2019-03-06 23:28:31 -0800

    Trimmed the branch to only the network_detector sub directory

--- multi_interface_roam/src/multi_interface_roam/sigblock_build.py
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env python
-
-from distutils.core import setup, Extension
-
-setup(name = "sigblock",
-        version = "1.0",
-        ext_modules = [Extension("sigblock", ["sigblock.c"])])
