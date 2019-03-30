cb2a93bfa87fa07b12de582fcd562c0c031e4963
David Feil-Seifer <dave@cse.unr.edu>
2019-03-30 10:48:04 -0700

    Trimmed the branch to only the linux_networking sub directory

--- network_control_tests/test/integrated_tests/bitrate_test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-PACKET_SIZES=(150 1500 3000 6000 12000 24000 48000)
-
-for PKTSIZE in ${PACKET_SIZES[@]}; do
-  rosrun network_monitor_udp bitrate_test.py $PKTSIZE > bitrate_result_$PKTSIZE.plot
-done
